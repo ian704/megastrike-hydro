6bc8aaf3076fa7aa632ee51669b50fb52a780ae6
ian704 <ianwanganga891@gmail.com>
2026-03-19 22:07:15 +0300

    Fix SSL connection for Render Postgres

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ const { Pool } = require('pg');
 // Create pool using Render DATABASE_URL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // ==========================
