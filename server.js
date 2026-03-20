ce5cf9cf6c4bedaa63107d09104e6576366e1ec2
ian704 <ianwanganga891@gmail.com>
2026-03-20 14:16:29 +0300

    Fix duplicate path declaration in server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('./db');
