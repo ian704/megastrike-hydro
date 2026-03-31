090830fd054266dda0142ef728634dbc94785b87
ian704 <ianwanganga891@gmail.com>
2026-03-31 18:16:28 +0300

    fix smtp email timeout using port 465

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const app = express();
 // EMAIL CONFIGURATION
 // ==========================
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
